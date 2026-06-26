1130de921e1e7515649627684a98e52284622908
Bazilur Rahman Bazigh <bazilur.rb@gmail.com>
2026-06-26 23:45:31 +0530

    Added Login Logout, register Features, and Routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRouter from './routes/authRoutes.js';
+
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Server is running...'));
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
